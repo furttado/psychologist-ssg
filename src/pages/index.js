792e9d1886e0bb0e0fdc9e8e0ef3ca25a599097c
furttado <furttado@gmail.com>
2023-03-03 13:21:25 -0300

    tasks: SEO, head, meta tags, Header icons space

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,17 @@ import MainComponent from '../components/mainComponent'
 import ContactComponent from '../components/contactComponent'
 import CasesComponent from '../components/casesComponent'
 import FooterComponent from '../components/footerComponent'
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+  <title>Kelly Mara Psicóloga</title>
+  <meta name="Kelly, Psicóloga em Muriaé-MG - Tratamento para problemas emocionais e psicológicos - Atendimento presencial e online - Consulta com psicóloga experiente - Agendamento pelo WhatsApp" content="Kelly é uma psicóloga experiente que oferece tratamento presencial e online para diversos problemas emocionais e psicológicos em Muriaé-MG. Agende sua consulta pelo WhatsApp e receba ajuda profissional de uma psicóloga experiente." />
+  <link rel='icon' href='/favicon2.ico' />
+</Head>
     <GlobalStyle />
    <Container>
      <HeaderComponent/>
